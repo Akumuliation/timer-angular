3eb0ae58dd715bf2525d9be97a34f1228ddb7582
vetal <akumuliation@gmail.com>
2018-06-27 20:18:36 +0300

    Fixed Wait button

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, BehaviorSubject } from 'rxjs';
 
 
 @Component({
@@ -13,60 +13,50 @@ export class TimerComponent implements OnInit, OnDestroy {
   private subscriptionTimer: Subscription;
   private startSeconds: number;
   private totalSeconds: number;
+  private timeClick = new BehaviorSubject<number>(0);
 
   public hh: number;
   public mm: number;
   public ss: number;
   public isStart: boolean;
+  public isReset: boolean;
+  public isEnd: boolean;
 
   constructor() { }
 
-  ngOnInit() {
-
-    this.setSeconds();
-
-    this.print(this.startSeconds);
-    this.isStart = false;
-  }
-
-  ngOnDestroy() {
-
-    this.unsubscribeTime();
-
-  }
-
-  private setSeconds() {
+  private setSeconds(): void {
 
     this.totalSeconds = this.startSeconds = +this.time['second'] + this.time['minute'] * 60 + this.time['hour'] * 60 * 60;
 
   }
 
-  private print( seconds ) {
+  private print( seconds ): void {
 
     const date = new Date(seconds * 1000);
-          this.hh = date.getUTCHours();
-          this.mm = date.getUTCMinutes();
-          this.ss = date.getSeconds();
+    this.hh = date.getUTCHours();
+    this.mm = date.getUTCMinutes();
+    this.ss = date.getSeconds();
+
   }
 
-  private subscribeTime() {
+  private subscribeTime(): void {
 
     this.subscriptionTimer = interval(1000).subscribe(() => {
-
-      if ( this.totalSeconds === 0 ) {
-        this.unsubscribeTime();
-        return;
-      }
 
       this.totalSeconds = this.totalSeconds - 1;
 
       this.print( this.totalSeconds );
 
+      if ( this.totalSeconds === 0 ) {
+        this.isEnd = true;
+        this.unsubscribeTime();
+      }
+
     });
 
   }
 
-  private unsubscribeTime() {
+  private unsubscribeTime(): void {
 
     if ( this.subscriptionTimer ) {
 
@@ -76,26 +66,61 @@ export class TimerComponent implements OnInit, OnDestroy {
 
   }
 
-  public reset() {
+  public reset(): void {
 
     this.unsubscribeTime();
 
     this.totalSeconds = this.startSeconds;
     this.print(this.totalSeconds);
-    this.isStart = true;
+    this.isStart = false;
+    this.isReset = false;
+    this.isEnd = false;
+
   }
 
-  public start() {
+  public start(): void {
 
     this.subscribeTime();
-
     this.isStart = true;
+    this.isReset = true;
+
   }
 
-  public stop() {
+  public stop(): void {
+
+    this.unsubscribeTime();
+    this.isStart = false;
+
+  }
+
+  public wait(): void {
+
+    let currentTime = new Date().getTime();
+    const delay = currentTime - this.timeClick.getValue();
+
+    if ( delay < 300 ) {
+
+      this.stop();
+      currentTime = 0;
+
+    }
+
+    this.timeClick.next(currentTime);
+  }
+
+  public ngOnInit(): void {
+
+    this.setSeconds();
+    this.print(this.startSeconds);
+    this.isStart = false;
+    this.isReset = false;
+    this.isEnd = this.startSeconds === 0;
+
+  }
+
+  public ngOnDestroy(): void {
 
     this.unsubscribeTime();
 
-    this.isStart = false;
   }
 }
